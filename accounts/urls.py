60dd6965bf906e8094d3771dd5057d045439ce17
IvanGaidukevich <gaydu7@gmail.com>
2025-03-03 18:34:38 +0300

    add forms

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,7 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('profile/', views.profile_view, name='profile_view'),
+    path('login/', views.login_view, name='login_view'),
+    path('logout/', views.logout_view, name='logout_view'),
+    path('register/', views.register_view, name='register_view'),
 ]
